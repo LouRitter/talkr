b78421e036dbc0aea7ba1f1d16b79a08bb600b7c
louritter <louritter1@gmail.com>
2022-03-12 09:19:10 -0500

    added abbility to make shouts

---
+++ app/controllers/shouts_controller.rb
@@ -0,0 +1,21 @@
+class ShoutsController < ApplicationController
+  
+  def create
+    shout = current_user.shouts.create(shout_params)
+    redirect_to :root, redirect_options_for(shout)
+  end
+
+  private 
+
+  def shout_params
+    params.require(:shout).permit(:body)
+  end
+
+  def redirect_options_for(shout)
+    if shout.persisted? 
+      {notice: "Shouted Successfully"}
+    else
+      {alert: "Invalid Shout"}
+    end
+  end
+end
